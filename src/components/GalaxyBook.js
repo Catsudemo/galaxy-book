3280f957cc79040e330cc64f216a57d26e9c110e
InaSLew <ina.shenlewander@gmail.com>
2019-11-09 14:31:22 +0100

    Update0

---
+++ src/components/GalaxyBook.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PicturePage from './PicturePage';
+import NotePage from './NotePage';
+
+const GalaxyBook = () => {
+    return(
+        <div>
+            <PicturePage></PicturePage>
+            <NotePage></NotePage>
+        </div>
+    )
+}
+
+export default GalaxyBook;
---
+++ src/components/PicturePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PicturePage = () => {
+    return (
+        <div>
+            Stub from PicturePage
+        </div>
+    );
+}
+
+export default PicturePage;
